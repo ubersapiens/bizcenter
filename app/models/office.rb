aa83104b2e2ba17fc6e6382bc1109e15d3cff6cf
JFP3 <jfpardini@gmail.com>
2014-09-18 17:19:08 +0200

    Geocoder added. Model and DB adapted.

--- app/models/office.rb
+++ app/models/office.rb
@@ -1,5 +1,6 @@
 class Office < ActiveRecord::Base
 	mount_uploader :image, ImageUploader
-
   	belongs_to :user
+  	geocoded_by :full_street_address   
+	after_validation :geocode          
 end
---
+++ db/migrate/20140918151643_add_latitude_and_longitude_to_offices.rb
@@ -0,0 +1,6 @@
+class AddLatitudeAndLongitudeToOffices < ActiveRecord::Migration
+  def change
+    add_column :offices, :latitude, :float
+    add_column :offices, :longitude, :float
+  end
+end
