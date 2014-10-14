cc84ddb3a888767eaa7a7900c136ee3eadc2ae73
JFP3 <jfpardini@gmail.com>
2014-10-14 14:56:25 +0200

    CanCan added.

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem 'rails', '4.1.1'
 
 gem 'autoprefixer-rails'
 gem 'bootstrap-sass', '~> 3.2.0'
+gem 'cancan'
 gem 'carrierwave'
 gem 'coffee-rails', '~> 4.0.0'
 gem 'devise'
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  
 	root 'sites#homepage'
 
 	resources :offices
 
 	devise_for :user, :path => '', :path_names => { :sign_in => "login", :sign_out => "logout", :sign_up => "register" }
+
 end
